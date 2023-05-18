cd89798e778f6bce8dfe5a6c0bd411122eb4fb11
marchmichels <56034198+marchmichels@users.noreply.github.com>
2023-05-18 17:42:55 -0400

    Create heart button

--- src/components/InfiniteTweetList.tsx
+++ src/components/InfiniteTweetList.tsx
@@ -1,4 +1,8 @@
+import Link from "next/link"
 import InfiniteScroll from "react-infinite-scroll-component"
+import { ProfileImage } from "./ProfileImage"
+import { useSession } from "next-auth/react"
+import { VscHeart, VscHeartFilled } from "react-icons/vsc"
 
 type Tweet = {
     id: string
@@ -36,9 +40,68 @@ export function InfiniteTweetList({ tweets, isError, isLoading, fetchNewTweets, 
                 loader={"Loading..."}
             >
                 {tweets.map(tweet => {
-                    return <div key={tweet.id}>{tweet.content}</div>
+                    return <TweetCard key={tweet.id} {...tweet} />;
                 })}
             </InfiniteScroll>
         </ul>
-    )
+    );
+}
+
+const dateTimeFormatter = new Intl.DateTimeFormat(undefined, { dateStyle: "short" })
+
+function TweetCard({ id, user, content, createdAt, likeCount, likedByMe }: Tweet) {
+    return (
+
+        <li className="flex gap-4 border-b px-4 py-4">
+            <Link href={`/profiles/${user.id}`}>
+                <ProfileImage src={user.image} />
+            </Link>
+
+            <div className="flex flex-grow flex-col">
+                <div className="flex gap-1">
+                    <Link href={`/profiles/${user.id}`} className="font-bold outline-none hover:underline focus-visible:underline">{user.name}</Link>
+
+                    <span className="text-gray-500">-</span>
+                    <span className="text-gray-500">{dateTimeFormatter.format(createdAt)}</span>
+
+                </div>
+                <p className="whitespace-pre-wrap">{content}</p>
+                <HeartButton likedByMe={likedByMe} likeCount={likeCount} />
+            </div>
+
+        </li>
+
+
+    );
+};
+
+type HeartButtonProps = {
+    likedByMe: boolean
+    likeCount: number
+}
+
+function HeartButton({ likedByMe, likeCount }: HeartButtonProps) {
+
+    const session = useSession()
+    const HeartIcon = likedByMe ? VscHeartFilled : VscHeart
+
+    if (session.status !== "authenticated") {
+        return (
+            <div className="mb-1 mt-1 flex items-center gap-3 self-start text-gray-500">
+                <HeartIcon />
+                <span>{likeCount}</span>
+            </div>
+        )
+    };
+
+    return (
+        <button className={`group items-center gap-1 self-start flex transition-colors duration-200 ${likedByMe ? "text-red-500" : "text-gray-500 hover:text-red-500 focus-visible:text-red-500"}`}>
+            <HeartIcon className={`transition-colors duration-200 ${likedByMe
+                ? "fill-red-500"
+                : "group-hover:fill-red-500 fill-gray-500 group-focus-visible:fill-red-500"
+                }`}
+            />
+            <span>{likeCount}</span>
+        </button>
+    );
 }
